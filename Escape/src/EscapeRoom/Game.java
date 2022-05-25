fda5a2478ad593761c77bcd4ba6c5d3f62d2fad1
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-05-25 21:21:46 +0300

    Renamed some files
    Renamed some files

--- Escape/src/EscapeRoom/Game.java
+++ Escape/src/EscapeRoom/Game.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package escape;
+package EscapeRoom;
 
 /**
  *
  * @author Archontis
  */
-public class Escape {
+public class Game {
 
     /**
      * @param args the command line arguments
