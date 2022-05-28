1b2d853aafcc7dbda3aceeb9d73cedf55a85dcb5
AlexandraKoudouni <103773093+AlexandraKoudouni@users.noreply.github.com>
2022-05-28 12:11:32 +0300

    Γενικό πλάνο κλάσης.

--- Escape/src/EscapeRoom/Riddles.java
+++ Escape/src/EscapeRoom/Riddles.java
@@ -9,6 +9,7 @@ package EscapeRoom;
  * @author 30694
  */
 import java.util.*;
+import java.awt.Image;
 
 public class Riddles {
     private Storyteller storyteller;
@@ -19,5 +20,33 @@ public class Riddles {
         this.riddleId = riddleId;
     }
     
+    public boolean pictures_in_a_row(ArrayList<Image> images){
+        boolean solved = false;
+        return solved;
+    }
+    
+    public boolean match_image_to_text(ArrayList<Image> images , ArrayList<String> text){
+        boolean solved = false;
+        return solved;
+    }
+    public boolean multiple_choice(ArrayList<String> question , ArrayList<String> answear){
+        boolean solved = false;
+        return solved;
+    }
+    
+    public boolean type_the_answear(String Question){
+        boolean solved = false;
+        return solved;
+    }
+    
+    public boolean crossword_puzzle(ArrayList<String> clues){
+        boolean solved = false;
+        return solved;
+    }
+    
+    public boolean puzzle(){
+        boolean solved = false;
+        return solved;
+    }
     
 }
