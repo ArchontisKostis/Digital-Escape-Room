3f68e08a4aa2783681c84bde9570089c6ff31c57
AlexandraKoudouni <103773093+AlexandraKoudouni@users.noreply.github.com>
2022-05-28 11:48:51 +0300

    Storyteller & Riddles classes

---
+++ Escape/src/EscapeRoom/Riddles.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EscapeRoom;
+
+/**
+ *
+ * @author 30694
+ */
+import java.util.*;
+
+public class Riddles {
+    private Storyteller storyteller;
+    private int riddleId;
+    private ArrayList<Image> riddleImages = new ArrayList<Image>();
+    
+    public Riddles(int riddleId){
+        this.riddleId = riddleId;
+    }
+    
+    
+}
