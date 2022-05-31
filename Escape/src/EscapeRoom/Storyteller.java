2ee39e3162fecfaef1e3f96e70fe47ef220598a0
christsarcho <ics21072@uom.edu.gr>
2022-05-31 17:14:32 +0300

    Update Storyteller.java

--- Escape/src/EscapeRoom/Storyteller.java
+++ Escape/src/EscapeRoom/Storyteller.java
@@ -21,11 +21,18 @@ public class Storyteller {
     private String Gmsg4 = "Πρέπει να γράψεις τη λέξη που βγάζουν τα έντονα γράμματα του κειμένου";
     private String Gmsg5 = "Ποιο είναι το ον που όταν γεννιέται έχει τέσσερα πόδια, στη συνέχεια γίνεται δίποδο και όταν γερνά αποκτά και τρίτο πόδι;";
     
-    /*Ερωτήσεις για τους Αιγυπτιακόυς γριφους*/
+    /*Ερωτήσεις για τους Αιγυπτιακόυς γρίφους*/
     private String Emsg1 = "Να βρείς και να πατήσεις με τη σωστή σειρά πανω στα εξεις σύμβολα";
     private String Emsg2 = "Ποιός είναι ο θεός της καταστροφής;";
     private String Emsg3 = "Πρέπει να λύσεις το σταυρόλεξο για να βρείς τον κωδικό του γρίφου";
-    private String Emsg4 = "Πρέπει να μετρήσεις το πλήθος των εικόνων που υπάρχουν στην παρακάτω φοτογραφία";       
+    private String Emsg4 = "Πρέπει να μετρήσεις το πλήθος των εικόνων που υπάρχουν στην παρακάτω φοτογραφία";
+    
+    /*Ερωτήσεις για τους Σκανδιναβικούς γρίφους*/
+    private String Smsg1 = "Πρέπει να βάλεις τις εικόνες στη σωστή σειρά";
+    private String Smsg2 = "Απάντησε στις ερωτήσεις πολλαπλής επιλογής";
+    private String Smsg3 = "Πρεπει να σχεδιάσεις την διαδρομή που θα ακολουθήσει ο Thor για να νικήσει το φίδη. Υπάρχουν δύο περιορισμοί: \n"
+            + "Η διαδρομή πρέπει να διατρέχει ολόκληρο το σώμα του φιδιού \n"
+            + "Δεν μπορείς να ξαναπεράσεις απο το ίδιο σημείο δ΄υο φορές. Μπορείς όμως να ξαναπεράσεις απο τους κόμβους";
             
     public Storyteller(String riddleHelp , String riddleStory){
         this.riddleHelp = riddleHelp;
@@ -53,17 +60,28 @@ public class Storyteller {
     }
     
     public String ShowEgyMsg2(){
-        return Gmsg2;
+        return Emsg2;
     }
         
     public String ShowEgyMsg3(){
-        return Gmsg3;
+        return Emsg3;
     }
             
     public String ShowEgyMsg4(){
-        return Gmsg4;
+        return Emsg4;
     }
-         
+    
+    public String ShowScanMsg1(){
+        return Smsg1;
+    }
+    
+    public String ShowScanMsg2(){
+        return Smsg2;
+    }
+    
+    public String ShowScanMsg3(){
+        return Smsg3;
+    }
  
     /*
     public void Give_Help(Storyteller storyteller){
