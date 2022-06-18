5a1c05bba04e50bd077935125c7fc38f90bed82b
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-06-18 17:27:37 +0300

    Update Game.java

--- Escape/src/EscapeRoom/Game.java
+++ Escape/src/EscapeRoom/Game.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  * @author Archontis
  */
 public class Game {
-<<<<<<< HEAD
+
     // Properties Declaration
     private Player player;
     private ArrayList<Room> roomList;    // TO-DO: <Room>
@@ -19,14 +19,9 @@ public class Game {
     // Player
     public void setPlayer(Player aPlayer){
         this.player = aPlayer;
-=======
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
->>>>>>> alexandra
     }
+
+
     
     public Player getPlayer(){
         return this.player;
