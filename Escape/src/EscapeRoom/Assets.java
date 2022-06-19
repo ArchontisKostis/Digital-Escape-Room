f99060f40d422e2a00bb56b37455a22af03fe406
christsarcho <ics21072@uom.edu.gr>
2022-06-19 12:22:32 +0300

    Assets

---
+++ Escape/src/EscapeRoom/Assets.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EscapeRoom;
+
+/**
+ *
+ * @author giorg
+ */
+public class Assets extends Items{
+    
+
+
+    public Assets(String itemName, int itemId, int itemRiddle) {
+            super(itemName, itemId);        
+	}
+    
+
+
+}
