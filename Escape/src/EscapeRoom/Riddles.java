a8b1b6763d85ac37a3d24ceff8580bf7c2f7c6d8
AlexandraKoudouni <103773093+AlexandraKoudouni@users.noreply.github.com>
2022-06-19 12:14:50 +0300

    διορθώσεις σφαλμάτων μετά απο merge στη κλάση riddles

--- Escape/src/EscapeRoom/Riddles.java
+++ Escape/src/EscapeRoom/Riddles.java
@@ -12,19 +12,13 @@ import java.util.*;
 import java.awt.Image;
 
 public class Riddles {
-
     
-    private int riddleId;
-
-    private Storyteller storyteller;
     private int riddleId;
     private ArrayList<Image> riddleImages = new ArrayList<Image>();
 
-    
     public Riddles(int riddleId){
         this.riddleId = riddleId;
     }
-    
 
     public boolean pictures_in_order(int Order[]){
         boolean solved = true;
@@ -37,60 +31,51 @@ public class Riddles {
                 }
             }
         else if(riddleId == 0012)
-            solved = row.getImageTextGr().getOrder().equals(Order);
+            for(int i=0 ; i<row.getImageTextGr().getNumberOfQuestions() ; i++){
+                if(Order[i]!=row.getImageTextGr().getOrder()[i]){
+                    solved = false;
+                    break;
+                }
+            }
         else if(riddleId == 0021)
-            solved = row.getImageTextEg().getOrder().equals(Order);
+            for(int i=0 ; i<row.getImageTextEg().getNumberOfQuestions() ; i++){
+                if(Order[i]!=row.getImageTextEg().getOrder()[i]){
+                    solved = false;
+                    break;
+                }
+            }
         else if(riddleId == 0031)
-            solved = row.getRowSc().getOrder().equals(Order);
+            for(int i=0 ; i<row.getRowSc().getNumberOfImages() ; i++){
+                if(Order[i]!=row.getRowSc().getOrder()[i]){
+                    solved = false;
+                    break;
+                }
+            }
         else
-            solved = row.getTextToText().getOrder().equals(Order);
+            for(int i=0 ; i<3 ; i++){
+                if(Order[i]!=row.getTextToText().getOrder()[i]){
+                    solved = false;
+                    break;
+                }
+            }
             
         return solved;
     }
     
     public boolean multiple_choice(int position , int pos4 , int pos5 , int pos6){
-
-    
-    public boolean pictures_in_order(int riddleId , String Order){
-        boolean solved;
-        Data row = new Data();
-        if(riddleId == 1)
-            solved = (row.getRowGr()).getOrder().equals(Order);
-        else
-            solved = (row.getRowSk()).getOrder().equals(Order);
-        return solved;
-    }
-    
-    
-    /*
-    public boolean match_image_to_text(ArrayList<Image> images , ArrayList<String> text){
-        boolean solved = false;
-        return solved;
-    }
-    */
-    public boolean multiple_choice(int position , int riddleId , int pos4 , int pos5 , int pos6){
-
-        
         boolean solved;
         Data question = new Data();
-        
 
-        if(riddleId == 0013){            
+        if(riddleId == 0013){
             solved = position == (question.getQuestion1()).getRightAnswear();
         }
         else if (riddleId == 0022){
-
-        if(riddleId == 1){            
-            solved = position == (question.getQuestion1()).getRightAnswear();
-        }
-        else if (riddleId == 2){
-
             solved = position == (question.getQuestion2()).getRightAnswear();
         }
         else{
             solved = (position == ((question.getQuestion3()).getRightAnswear())) && (pos4 == (question.getQuestion4()).getRightAnswear()) && (pos5 == (question.getQuestion5()).getRightAnswear()) && (pos6 == (question.getQuestion6()).getRightAnswear());
         }
-        
+
         return solved;
     }
     
@@ -107,22 +92,7 @@ public class Riddles {
         }
         else{
             rightAnswear = text.getTextEg1()[1];
-
-    public boolean type_the_answear(String answear , int riddleId){
-        String rightAnswear;
-        Data text = new Data();
-        
-        if(riddleId == 1){
-            rightAnswear = (text.getText1())[1];
         }
-        else if(riddleId == 2){
-            rightAnswear = (text.getText2())[1];
-        }
-        else{
-            rightAnswear = text.getText3();
-
-        }
-        
         return rightAnswear.equals(answear);
     }
     
@@ -145,21 +115,5 @@ public class Riddles {
     
     public boolean crossword_check(int numOfCorrectAns){
         return numOfCorrectAns == 5;
-    } 
-
-    /*
-    public boolean crossword_puzzle(ArrayList<String> clues){
-        boolean solved = false;
-        return solved;
     }
-    */
-    
-    /*
-    public boolean puzzle(){
-        boolean solved = false;
-        return solved;
-    }
-    */
-    
-
 }
